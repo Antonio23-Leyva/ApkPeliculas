8aae709f88b5da79ca345d828ce61bc63db9b247
Antonio23-Leyva <luisa.leyva@potros.itson.edu.mx>
2021-09-27 22:58:49 -0700

    terminado

---
+++ app/src/main/java/luis/leyva/peliculas_00000132320/Pelicula.kt
@@ -0,0 +1,10 @@
+package luis.leyva.peliculas_00000132320
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class Pelicula (val id: Int, var nombre: String, var descripcion: String, var img: Int){
+
+}
